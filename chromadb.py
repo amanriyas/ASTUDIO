87404ca1bcc3a9a4a538f728a33b4df83a9a823b
amanriyas <153854025+amanriyas@users.noreply.github.com>
2025-03-01 21:33:32 +0400

    first commit

---
+++ chromadb.py
@@ -0,0 +1,7 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import chromadb 
+import os 
+from sentence_transformers import SentenceTransformer
+
+
